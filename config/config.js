a0dcccdaced060716923fab61dbfd8733656546f
zhouyajun <zhouyajun@zhouyajundeMacBook-Pro-2.local>
2018-11-06 00:10:25 +0800

    页面布局

--- config/config.js
+++ config/config.js
@@ -1,16 +1,23 @@
 export default {
-    singular:true,  // 约定存放页面代码的文件为page单数，，如果指定则为pages
-    plugins:[
+    singular: true,  // 约定存放页面代码的文件为page单数，，如果指定则为pages
+    plugins: [
         ['umi-plugin-react', {
             // 针对插件的一些定制
-            antd:true
+            antd: true
         }]
     ],
-    routes :[
+    routes: [
         {
-            path:'/',
-            component:'./HelloWorld'  //是相对于 page 目录的相对路径
+            path: '/',
+            component: '../layout',
+            routes: [
+                {
+                    path: 'helloworld',
+                    component: './HelloWorld'  //是相对于 page 目录的相对路径
+                }
+            ]
         }
+
     ]
 
 };
